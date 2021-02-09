d0b36c39ca2c3fed4450a51dc3fa45f7ae1f23fc
Naziullah Shawn <naziullahshawn135@gmail.com>
2021-02-09 20:33:52 +0600

    finish

--- app.js
+++ app.js
@@ -1,47 +1,57 @@
-const searchMeal = () =>{
-    const searchText = document.getElementById('search-field').value;
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
-    fetch(url)
-    .then(res => res.json())
-    .then(data => displayMeals(data.meals))
-}
-const displayMeals = meals =>{
-    const mealContainer = document.getElementById('meal-container');
-    meals.forEach(meal => {
-        const mealDiv = document.createElement('div');
-        mealDiv.className = 'col-md-4'
-        mealDiv.innerHTML = `
+const searchMeal = () => {
+  const searchText = document.getElementById("search-field").value;
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayMeals(data.meals));
+};
+const displayMeals = (meals) => {
+  const mealContainer = document.getElementById("meal-container");
+  mealContainer.innerHTML = "";
+  meals.forEach((meal) => {
+    const mealDiv = document.createElement("div");
+    mealDiv.className = "col-md-4 mb-4";
+    mealDiv.innerHTML = `
         <div>
-        <img style="width:300px;" src="${meal.strMealThumb}">
+        <img src="${meal.strMealThumb}">
         <h3>${meal.strMeal}</h3>
-        <button onclick="Ingredients('${meal.idMeal}')">Ingredients</button>
+        <button onclick="Ingredients('${meal.idMeal}')" class="btn btn-primary" >Ingredients</button>
         </div>
         `;
-        mealContainer.appendChild(mealDiv);
-    });     
-} 
-const Ingredients = list =>{
-    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${list}`
-    fetch(url)
-    .then(res => res.json())
-    .then(data => displayDetails(data.meals[0]));
-}
-const displayDetails = ingredients => {
-    const detailsDiv = document.getElementById('meal-details');
-    detailsDiv.className = 'd-flex justify-content-center'
-    detailsDiv.innerHTML = `
-    <div>
-    <img style="width:400px;" src="${ingredients.strMealThumb}">
-     <h2>Ingredients</h2>
-     <p>✅${ingredients.strIngredient1}</p>
-     <p>✅${ingredients.strIngredient2}</p>
-     <p>✅${ingredients.strIngredient3}</p>
-     <p>✅${ingredients.strIngredient4}</p>
-     <p>✅${ingredients.strIngredient5}</p>
-    </div>
-     `
+    mealContainer.appendChild(mealDiv);
+  });
+};
+const Ingredients = (list) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${list}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayDetails(data.meals[0]));
+};
+const displayDetails = (ingredients) => {
+  const detailsDiv = document.getElementById("meal-details");
+  detailsDiv.innerHTML ='';
+  detailsDiv.innerHTML = `
+    <div class="meal-box">
+     <img src="${ingredients.strMealThumb}">
+     <h1>${ingredients.strMeal}</h1>    
+     </div>
+     <ul class="ingredients-list">
+     <li><h2>Ingredients</h2><li>
+     <li><h5>${ingredients.strIngredient1}</li>
+     <li><h5>${ingredients.strIngredient2}</h5</li>
+     <li><h5>${ingredients.strIngredient3}</h5</li>
+     <li><h5>${ingredients.strIngredient4}</h5</li>
+     <li><h5>${ingredients.strIngredient5}</h5</li>
+     <li><h5>${ingredients.strIngredient6}</h5</li>
+     <li><h5>${ingredients.strIngredient7}</h5</li>
+     <li><h5>${ingredients.strIngredient8}</h5</li>
+     <li><h5>${ingredients.strIngredient9}</h5</li>
+     <li><h5>${ingredients.strIngredient10}</h5</li>
+    </ul>
 
-    // console.log(${ingredients.strIngredient});
-    // console.log(ingredients.strIngredient2);
-    // console.log(ingredients.strIngredient3);
-}
+     `;
+
+  // console.log(${ingredients.strIngredient});
+  // console.log(ingredients.strIngredient2);
+  // console.log(ingredients.strIngredient3);
+};
